b59d2c05ab1d5e03c9365083941d8ca4885862e5
bimacahyosaktiutomo <bimasakticahyoutomo@gmail.com>
2024-10-01 12:16:39 +0800

    Review 5

---
+++ Pertemuan5/project_django/framework/ais/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Students
+
+class StudentsForm(forms.ModelForm):
+    class Meta:
+        model = Students
+        fields = ['nim', 'name', 'email', 'phone_number', 'year', 'teacher']
